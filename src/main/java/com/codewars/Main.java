46740e3237409e8b33f4aa7a7a17128cbc42b3c4
RtCryo <46868152+RtCryo@users.noreply.github.com>
2019-09-04 10:50:29 +0200

    Recover a secret string from random triplets(4)

--- src/main/java/com/codewars/Main.java
+++ src/main/java/com/codewars/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
     public static void main(String[] args)  {
 
-        System.out.println(PerfectPower.isPerfectPower(2));
+        char[][] triplets = {
+                {'m','a','t'},
+                {'a','t','h'},
+                {'i','s','f'},
+                {'t','h','i'},
+                {'s','f','u'},
+                {'f','u','n'},
+                {'a','t','i'}
+        };
+
+        System.out.println(SecretDetective.recoverSecret(triplets));
 
     }
 }
--- src/main/java/com/codewars/SecretDetective.java
+++ src/main/java/com/codewars/SecretDetective.java
@@ -11,21 +11,97 @@
 
 package com.codewars;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SecretDetective {
     public static String recoverSecret(char[][] triplets) {
-        List<StringBuilder> list = new ArrayList<>();
 
-        for(char[] t : triplets){
-            StringBuilder str = new StringBuilder();
-            for(char z : t){
-                str.append(z);
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < triplets.length; i++) {
+            for (int j = 0; j < triplets[i].length; j++) {
+                map.put(triplets[i][j], (j + 1) * 100);
             }
-            list.add(str);
+        }
+        int count = triplets.length;
+
+        while (count > 0) {
+            for (int i = 0; i < triplets.length; i++) {
+                if (map.get(triplets[i][1]) != null && map.getOrDefault(triplets[i][0], 0) <= map.getOrDefault(triplets[i][1], 0)) {
+                    map.put(triplets[i][0], map.get(triplets[i][1]) + 10);
+                } else if (map.get(triplets[i][2]) != null && map.getOrDefault(triplets[i][1], 0) <= map.getOrDefault(triplets[i][2], 0)) {
+                    map.put(triplets[i][1], map.get(triplets[i][2]) + 10);
+                }
+            }
+            count--;
         }
 
-        return "ok";
+        StringBuilder str = new StringBuilder();
+
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        Map<Character, Integer> result = new LinkedHashMap<>();
+        for (Map.Entry<Character, Integer> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        for (Map.Entry entry : result.entrySet()) {
+            str.append(entry.getKey());
+        }
+
+        return str.toString();
     }
 }
+
+/*import java.util.*;
+
+// This is a topological sort problem.
+// This is _not_ a particularly fast algorithm, and isn't general, but it's straightforward.
+public class SecretDetective {
+
+    // Convert the input format to the data structure I want to use
+    private static Map<Character, Set<Character>> buildEdgeMap(char[][] triplets) {
+        Map<Character, Set<Character>> map = new HashMap<>();
+        for(char[] cs : triplets) {
+            for (char c : cs) {
+                if (!map.containsKey(c)) map.put(c, new HashSet<>());
+            }
+            map.get(cs[0]).add(cs[1]);
+            map.get(cs[1]).add(cs[2]);
+        }
+        return map;
+    }
+
+    // Find a node with no outgoing edges
+    private static char findLast(Map<Character, Set<Character>> map) {
+        for(Map.Entry<Character, Set<Character>> entry : map.entrySet()) {
+            if (entry.getValue().isEmpty()) {
+                return entry.getKey();
+            }
+        }
+        throw new RuntimeException("No end point");
+    }
+
+    // Remove a node from the graph
+    private static void remove(Map<Character, Set<Character>> map, Character c) {
+        map.remove(c);
+        for(Set<Character> cs : map.values()) {
+            cs.remove(c);
+        }
+    }
+
+    // Reconstruct the string
+    public String recoverSecret(char[][] triplets) {
+        StringBuilder builder = new StringBuilder();
+        Map<Character, Set<Character>> map = buildEdgeMap(triplets);
+
+        while(!map.isEmpty()) {
+            char last = findLast(map);
+            builder.insert(0, last);
+            remove(map, last);
+        }
+
+        return builder.toString();
+    }
+}*/
